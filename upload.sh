a329dc8d68065f087abaf9b97ce53a11cdbc29dc
吴孔亮 <ray_ios@163.com>
2019-09-30 11:32:35 +0800

    5

--- upload.sh
+++ upload.sh
@@ -4,10 +4,10 @@ VersionString=`grep -E 's.version.*=' BProject.podspec`
 VersionNumber=`tr -cd 0-9 <<<"$VersionString"`
 
 NewVersionNumber=$(($VersionNumber + 1))
-LineNumber=`grep -nE 's.version.*=' BProject.podspec | cut -d : -f1`
-sed -i "" "${LineNumber}s/${VersionNumber}/${NewVersionNumber}/g" BProject.podspec
-
-echo "current version is ${VersionNumber}, new version is ${NewVersionNumber}"
+#LineNumber=`grep -nE 's.version.*=' BProject.podspec | cut -d : -f1`
+#sed -i "" "${LineNumber}s/${VersionNumber}/${NewVersionNumber}/g" BProject.podspec
+#
+#echo "current version is ${VersionNumber}, new version is ${NewVersionNumber}"
 
 git add .
 git commit -am ${NewVersionNumber}
